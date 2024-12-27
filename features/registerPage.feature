c39ec06b8194be4fb9c62e60bb2aa176f4305d9d
noyonsarder <noyonsarder87@gmail.com>
2024-12-27 11:02:49 +0600

    QA-3: Complete Signup as a new user

--- features/registerPage.feature
+++ features/registerPage.feature
@@ -1,4 +1,3 @@
-@register
 Feature: Register Page
     Scenario: Submit the registration form without providing any input
         Given I navigate to the test site
@@ -11,3 +10,17 @@ Feature: Register Page
             | password        | Password is required         |
             | confirmPassword | Confirm Password is required |
             | ageConfirmation | Please check above checkbox  |
+ 
+    Scenario: Submit the registration form with required field
+        Given I navigate to the test site
+        When I click to the Register link
+        And I fill the following required field
+            | firstName       | lamiya               |
+            | lastName        | Soniya               |
+            | email           | lamiya+2@yopmail.com |
+            | phoneNumber     | 9101735448           |
+            | password        | Tim1234@             |
+            | confirmPassword | Tim1234@             |
+            | ageConfirmation | check                |
+        And I click on the Register button
+        Then the "Account Created Successfully" text should display on confirmation page
--- step_definitions/registerPage.js
+++ step_definitions/registerPage.js
@@ -2,17 +2,16 @@ const locators = require('../locators/register.json');
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 
-When('I click to the Register link', { timeout: 100 * 1000 },async function () {
+When('I click to the Register link', { timeout: 100 * 1000 }, async function () {
 
     await this.page.locator(locators.registerLink).click();
 });
 
-When('I click on the Register button',{ timeout: 100 * 1000 }, async function () {
+When('I click on the Register button', { timeout: 100 * 1000 }, async function () {
     await this.page.locator(locators.registerBtn).click();
-    await this.page.waitForTimeout(5000);
 });
 
-Then('the following text are displayed', { timeout: 100 * 1000 },async function (table) {
+Then('the following text are displayed', { timeout: 100 * 1000 }, async function (table) {
     const values = table.rowsHash();
     if (values.firstName) {
         const errMsgLocator = this.page.locator(`//div[contains(text(), 'First Name is required')]`);
@@ -50,3 +49,48 @@ Then('the following text are displayed', { timeout: 100 * 1000 },async function 
         expect(actualErrorMessage).toContain(values.ageConfirmation);
     }
 });
+
+/*
+And I fill the following required field
+        |firstName|lamiya|
+        |email|lamiya@yopmail.com|
+        |phoneNumber|+91 01735448122|
+        |password|Tim1234@|
+        |confirmPassword|Tim1234@|
+        |ageConfirmation|check|
+        And I click on the Register button
+        Then the '' text display on the Dashboard page
+
+        */
+When('I fill the following required field', { timeout: 100 * 1000 }, async function (table) {
+    const values = table.rowsHash();
+    if (values.firstName) {
+        await this.page.locator(locators.firstName).fill(values.firstName);
+    }
+    if (values.lastName) {
+        await this.page.locator(locators.lastName).fill(values.lastName);
+    }
+    if (values.email) {
+        await this.page.locator(locators.email).fill(values.email);
+    }
+    if (values.phoneNumber) {
+        await this.page.locator(locators.phoneNumber).fill(values.phoneNumber);
+    }
+    if (values.password) {
+        await this.page.locator(locators.password).fill(values.password);
+    }
+    if (values.confirmPassword) {
+        await this.page.locator(locators.confirmPassword).fill(values.confirmPassword);
+    }
+    if (values.ageConfirmation) {
+        await this.page.locator(locators.ageConfirmation).check();
+    }
+});
+
+Then('the {string} text should display on confirmation page',{timeout: 100 * 1000},async function(text){
+    const confirmation= await this.page.locator(locators.accountConfirmation);
+    const confirmationMsg= await confirmation.textContent();
+    console.log(`This is confirmation message:${confirmationMsg}`);
+    expect(confirmationMsg).toContain(text);
+
+})
