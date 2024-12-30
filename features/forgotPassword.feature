53a901e04188171e2bcaa9135abc970b095b1788
noyonsarder <noyonsarder87@gmail.com>
2024-12-30 20:13:56 +0600

    QA-11:Completed the forgot password scenario

---
+++ features/forgotPassword.feature
@@ -0,0 +1,18 @@
+@smoke
+Feature: Forgot Password
+    
+    Scenario: Submit the forgot password form with empty data
+        Given I navigate to the test site
+        And I click on the "Forgot password" link
+        Then The "Enter New Password" text should display
+        And I click on "Save New Password" button
+        Then The following text should display
+            | email           | Email is required             |
+            | password        | *Password is required         |
+            | confirmPassword | *Confirm Password is required |
+        When I click on the "Login" link
+        Then The "Log in" text should display
+        When I click  browser back
+        Then The "Enter New Password" text should display
+        When I click on the "Register" link
+        Then The "Register" text should display
---
+++ step_definitions/forgotPassword.js
@@ -0,0 +1,52 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+const locators = require('../locators/forgotPassword.json');
+
+When('I click on {string} button', async function (button) {
+    const element = locators.saveNewPwdbtn;
+    await this.page.locator(element).click();
+});
+
+Then('The following text should display', async function (table) {
+    const values = table.rowsHash();
+    
+    if (values.email) {
+        const element = locators.genericText.replace('{txt}',values.email);
+        const elementText = await this.page.locator(element).textContent();
+        expect(elementText).toContain(values.email);
+
+    } if (values.password) {
+        const element = locators.genenircExactText.replace('{txt}',values.password);
+        const elementText = await this.page.locator(element).textContent();
+        expect(elementText).toContain(values.password);
+
+    } if (values.confirmPassword) {
+        const element = locators.genenircExactText.replace('{txt}',values.confirmPassword);
+        const elementText = await this.page.locator(element).textContent();
+        expect(elementText).toContain(values.confirmPassword);
+    }
+
+});
+
+When('I click on the {string} link', async function (link) {
+    let element = locators.genericLink.replace('{txt}', link);
+    switch (link) {
+        case 'Login':
+            await this.page.locator(element).click();
+            break;
+        case 'Register':
+            await this.page.locator(element).click();
+            break;
+        case 'Forgot password':
+            await this.page.locator(element).click();
+            break;
+        default:
+            throw new Error(`We couldn't find any element: ${link}`);
+    }
+
+})
+
+When('I click  browser back', async function () {
+    await this.page.goBack();
+
+})
